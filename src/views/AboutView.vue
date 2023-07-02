30ecf25630dfb7b76a3111ee02a2e3f707226106
zuxeld <zuxeld@gmail.com>
2023-07-02 15:44:00 +0400

    start

---
+++ src/views/AboutView.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <input type="file"  @change="upload($event)" id="file-input">
+    <button type="button" @click="veiwFile">do</button>
+    <p>{{ result }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      result: '',
+      file: '',
+    }
+  },
+
+  mounted() {
+    
+  },
+  methods: {
+    upload(e){
+      // let data = new FormData();
+      this.file = e.target.files[0];
+    },
+    veiwFile(){
+      let reader = new FileReader();
+  
+      reader.readAsArrayBuffer(this.file);
+  
+      reader.onload = function() {
+        let view = new Uint16Array(reader.result);
+        console.log(view);
+  
+      };
+  
+      reader.onerror = function() {
+        console.log(reader.error);
+      };
+    },
+  }
+}
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
